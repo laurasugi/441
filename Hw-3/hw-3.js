e37f2f028935e90c75373484fa93dfb60e637df1
laurasugi <lauracsugiyama@gmail.com>
2018-02-14 00:38:23 -0700

    Update hw-3.js
    added #3

--- Hw-3/hw-3.js
+++ Hw-3/hw-3.js
@@ -85,7 +85,12 @@ FizzBuzz
 -> etc....
 
 */
-
+for (var x=1; x < 101; x++){   
+    if (x % 15 == 0) console.log("Fizzbuzz");
+    if (x % 3 == 0) console.log("Fizz");
+    if (x % 5 == 0) console.log("Buzz");
+    console.log(x);
+}
 
 
 
